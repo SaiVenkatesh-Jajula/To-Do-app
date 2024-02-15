f81e1a1f5d63b8ef33a6a297a9996d7b100d61fa
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-15 10:42:04 +0530

    Implement add todo GUI

--- gui.py
+++ gui.py
@@ -1,10 +1,20 @@
 import PySimpleGUI as sg
+import backend
 
 label = sg.Text("Enter your Task :")
-inputbox = sg.InputText(tooltip="Enter ToDo")
+inputbox = sg.InputText(tooltip="Enter ToDo",key="value")
 add = sg.Button("Add")
-window = sg.Window(title='ToDos List', layout=[[label, inputbox],[add]]) # this line is like "dsfa" some value for class str for sg window.
+window = sg.Window(title='ToDos List',
+                   layout=[[label, inputbox], [add], []],
+                   font=("Aerial", 20))
+# this line is like "dsfa" some value for class str for sg window.
 #layout is like widgets
-window.read()
+while 1:
+    event, item=window.read() # when we add item - window read got an event it consists data!("Add",{'key':'input'})
+    match event:
+        case "Add":
+            backend.appending(item['value'])
+        case sg.WIN_CLOSED: #when we close X button in GUI it should break the loop.!
+            break
 
 window.close()
